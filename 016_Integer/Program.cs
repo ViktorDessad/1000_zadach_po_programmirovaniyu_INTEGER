546838602386736f44db8b3862a76a5befd03514
Віктор Шереш <sheregi1000@gmail.com>
2019-05-02 17:57:33 +0300

    Comment

---
+++ 016_Integer/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _016_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число");
+            int primaryValue = Convert.ToInt32(Console.ReadLine());
+
+            if (primaryValue >= 100 && primaryValue < 999)
+            {
+                // Разбиваем число 345 на простые цифры 
+                int firstReminder = primaryValue % 10;                                          // 5
+                int secondReminder = (primaryValue % 100 - firstReminder) / 10;                 // 4
+                int thirdReminder = (primaryValue - firstReminder - secondReminder * 10) / 100; // 3          
+
+                int firstNumber = secondReminder * 10;                                          // 40
+                int secondNumber = firstReminder;                                               // 5
+                int thirdNumber = thirdReminder* 100;                                           // 300
+
+                int sum = firstNumber + secondNumber + thirdNumber;
+
+                Console.WriteLine("Новое число = {0}", sum);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели неправильное число");
+            }
+
+        }
+    }
+}
